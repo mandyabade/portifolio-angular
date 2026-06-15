8436e9dad2628a2923e5ef5af85128c5761af331
mandyabade <20241ctb0100049@estudantes.ifpr.edu.br>
2026-06-15 19:15:01 +0000

    Aula16: Cria componentes, navegacao com Material e rotas do Portfolio

--- portfolio-angular/src/app/app.ts
+++ portfolio-angular/src/app/app.ts
@@ -1,9 +1,16 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
