a2af95a991e157678bf01fa47a077764a1786a3e
mandyabade <20241ctb0100049@estudantes.ifpr.edu.br>
2026-06-29 23:47:51 +0000

    Aula16: Cria componentes, navegacao com Material e rotas do Portfolio

---
+++ portfolio-angular/src/app/catalogo/catalogo.ts
@@ -0,0 +1,29 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { TecnologiaService, Tecnologia } from '../tecnologia.service';
+
+@Component({
+  selector: 'app-catalogo',
+  imports: [MatCardModule],
+  templateUrl: './catalogo.html'
+})
+export class Catalogo implements OnInit {
+  private service = inject(TecnologiaService);
+
+  tecnologias: Tecnologia[] = [];
+  carregando = true;
+  erro = '';
+
+  ngOnInit() {
+    this.service.listar().subscribe({
+      next: (lista) => {
+        this.tecnologias = lista;
+        this.carregando = false;
+      },
+      error: () => {
+        this.erro = 'Falha ao carregar o catálogo.';
+        this.carregando = false;
+      }
+    });
+  }
+}
